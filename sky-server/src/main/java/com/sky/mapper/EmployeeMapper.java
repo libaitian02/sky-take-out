72bdf0894da9161afedfed91af3dbda3d3b09c47
libaitian02 <107201118+libaitian02@users.noreply.github.com>
2025-09-08 23:57:15 +0800

    苍穹外卖项目day01实现代码加了个插入员工

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +17,6 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    @Insert("insert into employee (name)")
+    void insert(Employee employee);
 }
