6478b73cc4ee1c13574b6159395eab181fddf122
lizewei <3236507560@qq.com>
2025-09-20 23:41:10 +0800

    微信小程序完成

--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -1,11 +1,13 @@
 package com.sky.controller.user;
 
+import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.UserLoginDTO;
 import com.sky.entity.User;
 import com.sky.properties.JwtProperties;
 import com.sky.result.Result;
 import com.sky.service.UserService;
 import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user/user")
@@ -27,18 +30,30 @@ public class UserController {
     @Autowired
     private JwtProperties jwtProperties;
 
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
     @PostMapping("/login")
     @ApiOperation("微信登录")
-    public Result<UserLoginDTO> login(@RequestBody UserLoginDTO userLoginDTO) {
+    public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
         log.info("微信用户登录：{}",userLoginDTO.getCode());
-        userService.wxLogin(userLoginDTO);
 
+        //微信登录
+        User user = userService.wxLogin(userLoginDTO);
 
-        //为微信用户生成Jwt令牌
-        HashMap<String, Object> claims = new HashMap<>();
+        //为微信用户生成jwt令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.USER_ID,user.getId());
+        String token = JwtUtil.createJWT(jwtProperties.getUserSecretKey(), jwtProperties.getUserTtl(), claims);
 
-        JwtUtil.createJWT(jwtProperties.getUserSecretKey(),jwtProperties.getUserTtl(),claims);
-        return null;
+        UserLoginVO userLoginVO = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(token)
+                .build();
+        return Result.success(userLoginVO);
     }
 
 }
